87c9b715bb66352f7e62097a0ad7289e3a168e7c
saadwaqas688 <saadwaqas688@gmail.com>
2022-06-14 08:44:29 +0500

    filtering of user in Users tab is added

--- src/components/Views/Users/viewAllUsers.jsx
+++ src/components/Views/Users/viewAllUsers.jsx
@@ -1,26 +1,27 @@
-import React, { useEffect, useState } from 'react'
-import { Box, Skeleton } from '@mui/material';
-import { getService, updateService } from '../../../services/services';
-import DataTable from '../../UI/DataTable/DataTable';
+import React, { useEffect, useState } from "react";
+import { Box, Skeleton, Typography } from "@mui/material";
+import { getService, updateService } from "../../../services/services";
+import DataTable from "../../UI/DataTable/DataTable";
 import Modal from "@mui/material/Modal";
-import SingleUserDetails from './singleUserDetails';
-import EditUser from './EditUser';
+import SingleUserDetails from "./singleUserDetails";
+import EditUser from "./EditUser";
+import SelectMui from "../../UI/SelectMui/SelectMui";
 const columns = [
-  { id: 'name', label: 'Name', minWidth: 170 },
-  { id: 'email', label: 'Email', minWidth: 100 },
-  { id: 'phone', label: 'Phone Number', minWidth: 100 },
-  { id: 'Details', label: 'View Details', minWidth: 170 },
+  { id: "name", label: "Name", minWidth: 170 },
+  { id: "email", label: "Email", minWidth: 100 },
+  { id: "phone", label: "Phone Number", minWidth: 100 },
+  { id: "Details", label: "View Details", minWidth: 170 },
   {
-    id: 'status',
-    label: 'Status',
+    id: "status",
+    label: "Status",
     minWidth: 170,
-    align: 'right',
+    align: "right",
   },
   {
-    id: 'Actions',
-    label: 'Actions',
+    id: "Actions",
+    label: "Actions",
     minWidth: 170,
-    align: 'right',
+    align: "right",
   },
 ];
 
@@ -30,124 +31,147 @@ const style = {
   left: "50%",
   transform: "translate(-50%, -50%)",
   width: "60%",
-   height:"60%",
-  overflow:'scroll',
+  height: "60%",
+  overflow: "scroll",
   bgcolor: "background.paper",
   border: "2px solid #fbbe36",
-  p:4,
+  p: 4,
   boxShadow: 24,
   borderRadius: 2,
 };
+
+const categories = [
+  { id: "2", value: "monthlySubscriber" },
+  { id: "3", value: "yearlySubscriber" },
+  { id: "4", value: "onDemand" },
+];
 export default function ViewAllUsers() {
   const [openModal, setOpenModal] = React.useState(false);
 
   const [EditRecord, setEditRecord] = React.useState(false);
 
   const [showDetails, setShowDetails] = React.useState(false);
-  
-  const [singleRecord, setSingleRecord] = useState()
-  function handleClose(){
-    setEditRecord(false)
-    setShowDetails(false)
-    setSingleRecord(null)
-    setOpenModal(false)
-    }
+  const [selectedValue, setSelectedValue] = useState("All");
+  const [filterData, setFilterData] = useState(null);
 
-  
-    const [records, setRecords] = useState()
-    const [loading, setLoading] = useState(false)
-    const getAllUsers = async() => {
-        setLoading(true)
-        let list=[];      
-      const querySnapshot =await getService("Users")
+  const [singleRecord, setSingleRecord] = useState();
+  function handleClose() {
+    setEditRecord(false);
+    setShowDetails(false);
+    setSingleRecord(null);
+    setFilterData(null)
+    setOpenModal(false);
+    setSelectedValue("All")
 
-      querySnapshot.forEach((doc) => {
-        list.push({id:doc.id,
-          ...doc.data()})
-            });
-      
-      setRecords(list)
-      setLoading(false)
-
-      
-      };
-
-    useEffect(()=>{
-          getAllUsers()
-       },[ ])
-
-  
-    const updateStatus = async (id,status) => {
-      const newStatus= status==="blocked"?"active":"blocked";
-      await updateService('Users',id,{status:newStatus})
-      const updatedData = records.map((item) => {
-          
-        if(item.id === id){
-            item.status=newStatus
-        } 
-        return item
-        
-        });   
-      setRecords(updatedData) 
   }
-    return (<>          
 
-    {   loading ? (
+  const [records, setRecords] = useState();
+  const [loading, setLoading] = useState(false);
+  const getAllUsers = async () => {
+    setLoading(true);
+    let list = [];
+    const querySnapshot = await getService("Users");
+
+    querySnapshot.forEach((doc) => {
+      list.push({ id: doc.id, ...doc.data() });
+    });
+
+    setRecords(list);
+    setLoading(false);
+  };
+
+  useEffect(() => {
+    getAllUsers();
+  }, []);
+
+  async function updateStatus(id, status) {
+    const newStatus = status === "blocked" ? "active" : "blocked";
+    await updateService("Users", id, { status: newStatus });
+    let updatedData = records;
+    updatedData = updatedData.map((item) => {
+      if (item.id === id) {
+        item.status = newStatus;
+      }
+      return item;
+    });
+    setRecords(updatedData);
+  }
+
+  const handleChange = (event) => {
+    const value = event.target.value;
+    setSelectedValue(value);
+    if (value === "All") {
+      setFilterData(null);
+    } else {
+      const result = records.filter((i) => i.typeOfUser === value);
+      setFilterData(result);
+    }
+  };
+
+  return (
+    <>
+      {loading ? (
         <>
-              <Skeleton variant="text" height={100} />
-              <Skeleton variant="text" height={20} />
-              <Skeleton variant="text" height={20} />
-              <Skeleton variant="rectangular" height={300} />
+          <Skeleton variant="text" height={100} />
+          <Skeleton variant="text" height={20} />
+          <Skeleton variant="text" height={20} />
+          <Skeleton variant="rectangular" height={300} />
         </>
-    ):
-
-        records ?
+      ) : records ? (
         <>
-         <Modal
+          <Modal
             open={openModal}
             onClose={handleClose}
             aria-labelledby="modal-modal-title"
             aria-describedby="modal-modal-description"
           >
             <Box sx={style}>
-            { showDetails ?
-                <SingleUserDetails data={singleRecord}/>
-                       :
-                EditRecord?
+              {showDetails ? (
+                <SingleUserDetails data={singleRecord} />
+              ) : EditRecord ? (
                 <EditUser
-                
-                records={records}
-                setRecords={setRecords}
-                handleModal={handleClose}
-                getAllUsers={getAllUsers}
-                recordForEdit={singleRecord}
-                /> 
-                :
+                  records={records}
+                  setRecords={setRecords}
+                  handleModal={handleClose}
+                  getAllUsers={getAllUsers}
+                  recordForEdit={singleRecord}
+                />
+              ) : (
                 <></>
-                
-              }
+              )}
             </Box>
-          </Modal>    
-        <DataTable 
-        columns={columns}
-         rows={records}
-         editButton={true}
-        updateStatus={updateStatus}
-        setOpenModal={setOpenModal}
-        setEditRecord={setEditRecord}
-        setShowDetails={setShowDetails}
-        setSingleRecord={setSingleRecord}
-        
-         />
-           </>:
-          <></>
-    
-    }
-    
-    
-            </>
-    
-        )
-    }
-    
-    
+          </Modal>
+          <Box
+            sx={{
+              display: "flex",
+              justifyContent: "flex-start",
+              alignItems: "center",
+            }}
+          >
+            <SelectMui
+              options={categories}
+              handleChange={handleChange}
+              selectedValue={selectedValue}
+            />
+            <Typography sx={{ fontSize: "18px", fontWeight: "bold" }}>
+              Number of Users {filterData ? filterData.length : records.length}
+            </Typography>
+          </Box>
+
+          <DataTable
+            columns={columns}
+            rows={filterData ? filterData : records}
+            editButton={true}
+            updateStatus={updateStatus}
+            setOpenModal={setOpenModal}
+            setEditRecord={setEditRecord}
+            setShowDetails={setShowDetails}
+            setSingleRecord={setSingleRecord}
+          />
+        </>
+      ) : (
+        <></>
+      )}
+    </>
+  );
+}
